5ce8c999a9d8b828ebe5fde36c59686caf4bd470
Jose Cuevas <josecueram@gmail.com>
2020-06-20 14:59:26 -0500

    created basic MongoLib

--- config/index.js
+++ config/index.js
@@ -4,7 +4,8 @@ const config = {
   port: process.env.PORT || 8000,
   dbName: process.env.DB_NAME,
   dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD
+  dbPassword: process.env.DB_PASSWORD,
+  dbHost: process.env.DB_HOST
 }
 
 module.exports = config
