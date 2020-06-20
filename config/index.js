13471d4f920ca89e1393389a128a1319b1725260
Jose Cuevas <josecueram@gmail.com>
2020-06-20 14:43:24 -0500

    added db env variables

--- config/index.js
+++ config/index.js
@@ -1,7 +1,10 @@
 require('dotenv').config()
 
 const config = {
-  port: process.env.PORT || 8000
+  port: process.env.PORT || 8000,
+  dbName: process.env.DB_NAME,
+  dbUser: process.env.DB_USER,
+  dbPassword: process.env.DB_PASSWORD
 }
 
 module.exports = config
