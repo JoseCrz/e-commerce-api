ec4b3bf05f65dc8367506c7442a8c4ef0c444436
Jose Cuevas <josecueram@gmail.com>
2020-06-19 21:16:03 -0500

    created products view

--- routes/products.js
+++ routes/products.js
@@ -4,11 +4,13 @@ const router = express.Router()
 const products = [
   {
     name: 'Red Honey',
-    price: 75
+    price: 75,
+    image: 'https://cdn.shopify.com/s/files/1/0271/3472/2105/products/WhatsApp_Image_2020-06-04_at_11-removebg-preview_1296x.png?v=1591654871'
   },
   {
     name: 'Lavado',
-    price: 70
+    price: 70,
+    image: 'https://cdn.shopify.com/s/files/1/0271/3472/2105/products/WhatsApp_Image_2020-06-04_at_11-removebg-preview_1296x.png?v=1591654871'
   }
 ] 
 
