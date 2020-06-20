0ef1d84a1f2692666b310742fdfb9b722e7ed7d7
Jose Cuevas <josecueram@gmail.com>
2020-06-20 14:13:53 -0500

    added nice comments

--- index.js
+++ index.js
@@ -8,13 +8,16 @@ const productsAPIRouter = require('./routes/api/products')
 const  app = express()
 
 // ? Middleware
-
 app.use(express.json())
 
+// ? Static files settings
 app.use('/static', express.static(path.join(__dirname, 'static')))
+
+// ? View engine settings
 app.set('views', path.join(__dirname, 'views'))
 app.set('view engine', 'pug')
 
+// ? Routes
 app.use('/products', productsRouter)
 app.use('/api/products', productsAPIRouter)
 
