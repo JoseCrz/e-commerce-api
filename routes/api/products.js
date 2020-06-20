f41c3cde46bc903fbf43d0542f2ae22d3c1016a3
Jose Cuevas <josecueram@gmail.com>
2020-06-19 23:09:57 -0500

    added API Products Routes

--- routes/api/products.js
+++ routes/api/products.js
@@ -9,4 +9,38 @@ router.get('/', (req, res) => {
   })
 })
 
+router.post('/', (req, res) => {
+  res.status(201).json({
+    data: '13432353423',
+    message: 'product created'
+  })
+})
+
+router.get('/:productId', (req, res) => {
+  const { productId } = req.params
+
+  res.status('200').json({
+    data: productsMock[0],
+    message: 'product retrieved'
+  })
+})
+
+router.put('/:productId', (req, res) => {
+  const productData = req.body
+
+  res.status(200).json({
+    data: '12556543432',
+    message: 'product updated'
+  })
+})
+
+router.delete('/:productId', (req, res) => {
+  const { productId } = req.params
+
+  res.status(200).json({
+    data: '24654q232423',
+    message: 'product deleted'
+  })
+})
+
 module.exports = router
