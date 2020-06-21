f5f81100ced376bfb5ac6eb0d0b485e48f27ba4c
Jose Cuevas <josecueram@gmail.com>
2020-06-21 13:53:33 -0500

    created validationHandler middleware  base

---
+++ utils/middlewares/validationHandler.js
@@ -0,0 +1,12 @@
+const validate = (data, schema) => {
+  return false
+}
+
+const validationHandler = (schema, check = 'body') => {
+  return (req, res, next) => {
+    const error = validate(req[check], schema)
+    error ? next(new Error(error)) : next()
+  }
+}
+
+module.exports = validationHandler
